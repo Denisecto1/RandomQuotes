71ee51c44d8cfd4db8ae949272e6601f7d8d3ec1
matthewoboz <obozdenis@gmail.com>
2026-07-09 10:37:12 +0300

    Selecting quote from the array  of quotes, when the button is clicked

---
+++ script.js
@@ -0,0 +1,19 @@
+const quotes = [
+  'The only way to do great work is to love what you do. - Steve Jobs',
+  'Innovation distinguishes between a leader and a follower. - Steve Jobs',
+  "Your time is limited, so don't waste it living someone else's life. - Steve Jobs",
+];
+
+const quoteElement = document.getElementById('quote');
+const generateBtn = document.getElementById('generate-btn');
+
+function generateRandomQuote() {
+  console.log('THIS FUNCTION WAS JUST CALLED');
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
+  quoteElement.textContent = randomQuote;
+}
+// generateBtn.addEventListener();
+// generateRandomQuote();
+
+generateBtn.addEventListener('click', generateRandomQuote);
